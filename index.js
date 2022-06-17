7944b0cbafe8138bcd63a6bbf70baa62681d01a9
Dorian Loret <dorian.loret20@gmail.com>
2022-06-17 16:38:49 +0200

    save

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-// Créer la logique de compte à rebours
 
-choice.value = 5 minutes
+let totalSeconds;
+let interval;
 
-choice.value * 60 = totalSeconds
+function countdown () {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
 
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
+    const sec = seconds < 10 ? "0" + seconds : seconds;
 
-// Créer un événement à la validation du form pour lancer le compte à rebours
+    countdownDisplay.textContent = `${minutes} : ${sec}`;
+
+    if (totalSeconds > 0) {
+        totalSeconds--;
+    } else {
+        countdownDisplay.textContent = "C'est terminé !"
+        clearInterval(interval);
+    }
+
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    totalSeconds = choice.value * 60;
+    choice.value = "";
+    clearInterval(interval);
+    interval = setInterval(countdown, 1000);
+});
